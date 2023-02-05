b332af2e5c58eb5fc883c7d90954302369f14155
Hosaro <sty.hoareau@gmail.com>
2023-02-05 16:12:54 +0100

    05 Feb 2023

--- fondamentals.js
+++ fondamentals.js
@@ -60,4 +60,26 @@
 
 //          ----           ----           -----           ----            ----            ----            ---- 
 
+// --- 04 Feb 2023 ---
+// --- Instruction : 
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+// Return True if you're better, else False!
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+// --- My Solution : 
+// function betterThanAverage(classPoints, yourPoints) {
+//   const average = (classPoints.reduce((sum, e) => sum+e)+yourPoints)/(classPoints.length + 1)
+//   return average > yourPoints ? false : true
+// }
+
+// --- Best Practice : 
+// function betterThanAverage(classPoints, yourPoints) {
+//   return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
+// }
+
+//          ----           ----           -----           ----            ----            ----            ---- 
+
 
