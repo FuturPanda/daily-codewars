368051697f13f0e67ac07883ec24a98dd0e3c462
Hosaro <sty.hoareau@gmail.com>
2023-02-07 11:58:25 +0100

    07 Feb 2023

--- rank-up.js
+++ rank-up.js
@@ -9,10 +9,10 @@
 
 
 
-function humanReadable(seconds) {
-    let secs = seconds%60
-    let mins = seconds / 60
-    let hours = mins / 60
+// function humanReadable(seconds) {
+//     let secs = seconds%60
+//     let mins = seconds / 60
+//     let hours = mins / 60
     // mins < 1 ? mins = 00 : (mins = Math.floor(mins)  )
     // hours < 1 ? hours = 00 : hours = Math.floor(hours)
     // seconds%60 === 0 ? secs = 00 : secs = seconds%60 * 60 / 100
@@ -20,19 +20,130 @@ function humanReadable(seconds) {
 //     console.log(seconds%60)
 //     console.log (mins, hours)
 
-    console.log ( `${hours < 10 ? hours = '0'+hours : hours = hours }:${mins < 10 ? mins = 0+mins : mins = mins}:${secs < 10 ? secs = 0+secs : secs = secs}`)
+//     console.log ( `${hours < 10 ? hours = '0'+hours : hours = hours }:${mins < 10 ? mins = 0+mins : mins = mins}:${secs < 10 ? secs = 0+secs : secs = secs}`)
 
+// }
+
+
+// --- 6 Feb 2023 ---
+// --- Instruction : 
+// Write a function that when given a URL as a string, parses out just the domain 
+// name and returns it as a string. For example:
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+
+// --- My Solution : 
+// function domainName(url){
+//   let truncHttp, truncW
+//   if (url.includes("http://")) {
+//     truncHttp = url.slice(7)
+//   }
+//   else if(url.includes("https://")) {
+//     truncHttp = url.slice(8)
+//   }
+//   else truncHttp = url
+//   if (truncHttp.includes("www.")) {
+//     truncW = truncHttp.slice(4)
+//     return truncW.slice(0, truncW.indexOf("."))
+//   }
+//   else {
+//     return truncHttp.slice(0, truncHttp.indexOf("."))
+//   }
+// }
+
+// --- Best Practice : 
+// function domainName(url){
+//   url = url.replace("https://", '');
+//   url = url.replace("http://", '');
+//   url = url.replace("www.", '');
+//   return url.split('.')[0];
+// };
+
+//          ----           ----           -----           ----            ----            ----            ---- 
+
+
+
+const doTheMath = (obj) => {
+
+    const {rightOperand, operand, leftOperand} = obj
+    let result = 0
+   
+    if(operand === "plus"){
+        result = leftOperand + rightOperand
+    }
+    if(operand === "minus"){
+        result = leftOperand - rightOperand
+
+    }
+    if(operand === "dividedBy"){
+        result = Math.floor(leftOperand / rightOperand)
+
+    }
+    if(operand === "times"){
+        result = leftOperand * rightOperand
+    }
+    console.log("Do the math : " + result + ` operation : ${obj.leftOperand}${obj.operand}${obj.rightOperand} `)
+    return result
+}
+
+const checkThisOut = (nb, obj) => {
+    if(obj) {
+        obj.leftOperand = nb
+        return doTheMath(obj)
+    }
+    return {rightOperand : nb}
+}
+
+function zero(obj) {
+    return checkThisOut(0, obj)
+}
+function one(obj) {
+    return checkThisOut(1, obj)
+}
+function two(obj) {
+   return checkThisOut(2, obj)
+}
+function three(obj) {
+    return checkThisOut(3, obj)
+}
+function four(obj) {
+    return checkThisOut(4, obj)
+}
+function five(obj) {
+    return checkThisOut(5, obj)
+}
+function six(obj) {
+    return checkThisOut(6, obj)
+}
+function seven(obj) {
+    return checkThisOut(7, obj)
+}
+function eight(obj) {
+    return checkThisOut(8, obj)
+}
+function nine(obj) {
+    return checkThisOut(9, obj)
+}
+
+function plus(obj) {
+    obj.operand = "plus"
+    return obj
+}
+function minus(obj) {
+    obj.operand = "minus"
+    return obj
+}
+function times(obj) {
+    obj.operand = "times"
+    return obj
+}
+function dividedBy(obj) {
+    obj.operand = "dividedBy"
+    return obj
 }
 
 
 
-// humanReadable(0) // '00:00:00'
-// humanReadable(59) // '00:00:59'
-// humanReadable(60) // '00:01:00'
-humanReadable(90) // '00:01:30'
-// humanReadable(3599) // '00:59:59'
-// humanReadable(3600) // '01:00:00'
-// humanReadable(45296) // '12:34:56'
-// humanReadable(86399) // '23:59:59'
-// humanReadable(86400) // '24:00:00'
-// humanReadable(359999) // '99:59:59'
+console.log(eight(dividedBy(five())))
