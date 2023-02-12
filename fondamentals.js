0f407f5e9cc0b41b44b89535c475edb2338ae407
Hosaro <sty.hoareau@gmail.com>
2023-02-12 19:04:48 +0100

    12 Feb 2022 (1)

--- fondamentals.js
+++ fondamentals.js
@@ -103,3 +103,27 @@
 //          ----           ----           -----           ----            ----            ----            ---- 
 
 
+// --- 12 Feb 2022 ---
+// --- Instruction : 
+// Your goal in this kata is to implement a difference function, which 
+// subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b 
+// keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from 
+// the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+// --- My Solution : 
+// function arrayDiff(a, b) {
+//   return a.filter(item => b.includes(item) === false)
+// }
+
+// --- Best Practice : 
+// function array_diff(a, b) {
+//   return a.filter(e => !b.includes(e));
+// }
+
+//          ----           ----           -----           ----            ----            ----            ---- 
