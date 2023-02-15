8475b46c86201f7d7918a06fe1918f8453a4c0cb
Hosaro <sty.hoareau@gmail.com>
2023-02-15 12:14:03 +0100

    15 feb 23 -- failed

--- rank-up.js
+++ rank-up.js
@@ -570,3 +570,43 @@
 
 
 //          ----           ----           -----           ----            ----            ----            ---- 
+
+// --- 15 Feb 23 ---
+// --- Instruction : 
+// A friend of mine takes the sequence of all numbers from 1 to n (where n > 0).
+// Within that sequence, he chooses two numbers, a and b.
+// He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
+// Given a number n, could you tell me the numbers he excluded from the sequence?
+// The function takes the parameter: n (n is always strictly greater than 0) and returns an array or a string (depending on the language) of the form:
+
+// [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
+// with all (a, b) which are the possible removed numbers in the sequence 1 to n.
+
+// [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ... will be sorted in increasing order of the "a".
+
+// It happens that there are several possible (a, b). The function returns an empty array (or an empty string) if no possible numbers are found which will prove that my friend has not told the truth! (Go: in this case return nil).
+// --- My Solution : 
+// 
+// function removeNb (n) {
+//     let sum = ((n+1)/2)*n
+//     let nArray = []
+//     let result = []
+//     let b = 0 
+//      for (let j = Math.floor(n/2) ; j<n;j++){
+//         a = j
+//         for (let k = Math.floor(n/2); k<n ; k ++ ){
+//           b = k 
+//           if (a*b == sum -(a+b)) {
+//             result.push([a, b])
+//           }
+//         }
+//       }
+//       return result
+    
+//   }
+
+
+// --- Best Practice : 
+// 
+
+//          ----           ----           -----           ----            ----            ----            ---- 
