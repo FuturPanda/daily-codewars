b49048a7337171be4ea3c55c75a6c8988e864839
Hosaro <sty.hoareau@gmail.com>
2023-02-17 19:20:18 +0100

    17 feb 23 (failed)

--- rank-up.js
+++ rank-up.js
@@ -684,3 +684,109 @@
 // }
 //          ----           ----           -----           ----            ----            ----            ---- 
 
+
+// --- 17 Feb 23 ---
+// --- Instruction : 
+// Given a Sudoku data structure with size NxN, N > 0 and √N == integer, write a method to validate if it has been filled out correctly.
+
+// The data structure is a multi-dimensional Array, i.e:
+
+// [
+//   [7,8,4,  1,5,9,  3,2,6],
+//   [5,3,9,  6,7,2,  8,4,1],
+//   [6,1,2,  4,3,8,  7,5,9],
+  
+//   [9,2,8,  7,1,5,  4,6,3],
+//   [3,5,7,  8,4,6,  1,9,2],
+//   [4,6,1,  9,2,3,  5,8,7],
+  
+//   [8,7,6,  3,9,4,  2,1,5],
+//   [2,4,3,  5,6,1,  9,7,8],
+//   [1,9,5,  2,8,7,  6,3,4]
+// ]
+// Rules for validation
+
+// Data structure dimension: NxN where N > 0 and √N == integer
+// Rows may only contain integers: 1..N (N included)
+// Columns may only contain integers: 1..N (N included)
+// 'Little squares' (3x3 in example above) may also only contain integers: 1..N (N included)
+
+// --- My Solution : 
+// var Sudoku = function(data) 
+// {
+//   //   Private methods
+//   // -------------------------
+//   let testMethod = function() {
+//     for (let i=0; i<data.length;i++) {
+//         if (data.length !== data[i].length) return false
+//          const verifArray = new Set(data[i])
+//         if (verifArray.size !== data[i].length) return false
+//       } 
+//       let N = data.length
+//       let petitN = Math.sqrt(N)
+//       let carreArr = []
+//       for (let k=0; k<N ; k++)  {
+//         let toPush = []
+//         for (let i=0; i<petitN; i++) {
+//             for(let j=0 ; j<petitN; j++){
+//                 toPush.push(data[j].shift())
+//             }
+//         }
+//         carreArr.push(toPush)
+//         data.shift()
+//       }
+//       for (let x=0; x<carreArr.length; x++){
+//         const verifCarre = new Set(carreArr[x])
+//         console.log(verifCarre, " and ", carreArr[x])
+//         if (verifCarre.size !== carreArr[x].length) return false
+//       }
+//       return true
+//   }
+  
+  
+
+//   //   Public methods
+//   // -------------------------
+//   return {
+//     isValid: function() {
+      
+//       return testMethod()
+//     }
+//   };
+// };
+
+// var goodSudoku1 = new Sudoku([
+//     [7,8,4, 1,5,9, 3,2,6],
+//     [5,3,9, 6,7,2, 8,4,1],
+//     [6,1,2, 4,3,8, 7,5,9],
+  
+//     [9,2,8, 7,1,5, 4,6,3],
+//     [3,5,7, 8,4,6, 1,9,2],
+//     [4,6,1, 9,2,3, 5,8,7],
+    
+//     [8,7,6, 3,9,4, 2,1,5],
+//     [2,4,3, 5,6,1, 9,7,8],
+//     [1,9,5, 2,8,7, 6,3,4]
+//   ]);
+//   var badSudoku2 = new Sudoku([
+//     [1,2,3,4,5],
+//     [1,2,3,4],
+//     [1,2,3,4],  
+//     [1]
+//   ]);
+//   var badSudoku1 = new Sudoku([
+//     [1,2,3, 4,5,6, 7,8,9],
+//     [1,2,3, 4,5,6, 7,8,9],
+//     [1,2,3, 4,5,6, 7,8,9],
+    
+//     [1,2,3, 4,5,6, 7,8,9],
+//     [1,2,3, 4,5,6, 7,8,9],
+//     [1,2,3, 4,5,6, 7,8,9],
+    
+//     [1,2,3, 4,5,6, 7,8,9],
+//     [1,2,3, 4,5,6, 7,8,9],
+//     [1,2,3, 4,5,6, 7,8,9]
+//   ]);
+//   console.log(goodSudoku1.isValid())
+//   console.log(badSudoku1.isValid())
+
