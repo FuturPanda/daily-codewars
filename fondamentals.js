7a706495faf53facafbe9b067b8cf0fefbd431f0
Hosaro <sty.hoareau@gmail.com>
2023-02-22 10:59:28 +0100

    22 Feb 23 (fondamentals 1)

--- fondamentals.js
+++ fondamentals.js
@@ -2,36 +2,36 @@
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
-// My Solution 
+// My Solution
 // function squareSum(numbers){
 //     if(numbers.length == 0) return 0
 //     const arraySquared = numbers.map( e => e*e)
 //     console.log(arraySquared)
 //     const arraySum = arraySquared.reduce((sum, e) => sum + e)
 //       console.log(arraySum)
-  
+
 //     return arraySum
 //   }
 
-// Best Practise : 
+// Best Practise :
 // function squareSum(numbers){
 //     return numbers.reduce((sum,num) => sum + (num * num), 0);
 //   }
 
 //          ----           ----           -----           ----            ----            ----            ----
 
-// --- 03 February 2023 
+// --- 03 February 2023
 //Implement a function which convert the given boolean value into its string representation.
 //Note: Only valid inputs will be given.
 
-// My Solution : 
+// My Solution :
 // function booleanToString(b){
 //   let result
 //   b ? result ="true" : result = "false"
 //   return result
 // }
 
-// Best Practice : 
+// Best Practice :
 // function booleanToString(b){
 //   return b.toString();
 // }
@@ -43,25 +43,25 @@
 //          ----           ----           -----           ----            ----            ----            ----
 
 // --- 03 Feb 2023 ---
-// --- Instruction : 
-// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. 
+// --- Instruction :
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each.
 // If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
 // Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 
-// --- My Solution : 
+// --- My Solution :
 // function lovefunc(flower1, flower2){
 //   return flower1%2 === flower2%2 ? false : true
 // }
 
-// --- Best Practice : 
+// --- Best Practice :
 // function lovefunc(flower1, flower2){
 //   return flower1 % 2 !== flower2 % 2;
 // }
 
-//          ----           ----           -----           ----            ----            ----            ---- 
+//          ----           ----           -----           ----            ----            ----            ----
 
 // --- 04 Feb 2023 ---
-// --- Instruction : 
+// --- Instruction :
 // There was a test in your class and you passed it. Congratulations!
 // But you're an ambitious person. You want to know if you're better than the average student in your class.
 // You receive an array with your peers' test scores. Now calculate the average and compare your score!
@@ -69,67 +69,65 @@
 // Note:
 // Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 
-// --- My Solution : 
+// --- My Solution :
 // function betterThanAverage(classPoints, yourPoints) {
 //   const average = (classPoints.reduce((sum, e) => sum+e)+yourPoints)/(classPoints.length + 1)
 //   return average > yourPoints ? false : true
 // }
 
-// --- Best Practice : 
+// --- Best Practice :
 // function betterThanAverage(classPoints, yourPoints) {
-//   return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
+//   return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
 // }
 
-//          ----           ----           -----           ----            ----            ----            ---- 
+//          ----           ----           -----           ----            ----            ----            ----
 
 // --- 05 Feb 2023 ---
-// --- Instruction : 
+// --- Instruction :
 // Complete the solution so that it reverses the string passed into it.
 
-// --- My Solution : 
+// --- My Solution :
 // function solution(str){
 //   return str.split('').reverse().toString().replaceAll(',', '')
 // }
 
-// --- Best Practice : 
+// --- Best Practice :
 // function solution(str){
-//   return str.split('').reverse().join('');  
+//   return str.split('').reverse().join('');
 // }
 
-// --- Interessant : 
+// --- Interessant :
 // const solution = s => [...s].reverse().join('')
 
-
-//          ----           ----           -----           ----            ----            ----            ---- 
-
+//          ----           ----           -----           ----            ----            ----            ----
 
 // --- 12 Feb 2022 ---
-// --- Instruction : 
-// Your goal in this kata is to implement a difference function, which 
+// --- Instruction :
+// Your goal in this kata is to implement a difference function, which
 // subtracts one list from another and returns the result.
 
-// It should remove all values from list a, which are present in list b 
+// It should remove all values from list a, which are present in list b
 // keeping their order.
 
 // arrayDiff([1,2],[1]) == [2]
-// If a value is present in b, all of its occurrences must be removed from 
+// If a value is present in b, all of its occurrences must be removed from
 // the other:
 
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
-// --- My Solution : 
+// --- My Solution :
 // function arrayDiff(a, b) {
 //   return a.filter(item => b.includes(item) === false)
 // }
 
-// --- Best Practice : 
+// --- Best Practice :
 // function array_diff(a, b) {
 //   return a.filter(e => !b.includes(e));
 // }
 
-//          ----           ----           -----           ----            ----            ----            ---- 
+//          ----           ----           -----           ----            ----            ----            ----
 
 // --- 18 Feb 23 ---
-// --- Instruction : 
+// --- Instruction :
 // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
 // Examples:
@@ -139,19 +137,19 @@
 
 // Input: 123456789 Output: 987654321
 
-// --- My Solution : 
-// 
+// --- My Solution :
+//
 // function descendingOrder(n){
 //   return n<10 ? n : Number(String(n).split('').map(Number).sort((a,b) => b-a).join(""))
 // }
-// --- Best Practice : 
+// --- Best Practice :
 // function descendingOrder(n){
 //   return parseInt(String(n).split('').sort().reverse().join(''))
 // }
 
-//          ----           ----           -----           ----            ----            ----            ---- 
+//          ----           ----           -----           ----            ----            ----            ----
 // --- 18 Feb 23 ---
-// --- Instruction : 
+// --- Instruction :
 // The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
 
 // Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
@@ -162,8 +160,8 @@
 // rgb(255, 255, 300) // returns FFFFFF
 // rgb(0,0,0) // returns 000000
 // rgb(148, 0, 211) // returns 9400D3
-// --- My Solution : 
-// 
+// --- My Solution :
+//
 // function rgb(r, g, b){
 //   const letterArr = ["", "A", "B", "C", "D", "E", "F"]
 //   let arrRGB = [r, g, b]
@@ -191,8 +189,51 @@
 // }
 // rgb(300,255,255)
 
-// --- Best Practice : 
-// 
+// --- Best Practice :
+//
 
-//          ----           ----           -----           ----            ----            ----            ---- 
+//          ----           ----           -----           ----            ----            ----            ----
 
+// --- 22 Feb 23 ---
+// --- Instruction :
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+// --- My Solution :
+// function persistence(num) {
+//   if (num.toString().length == 1) return 0
+//   else {
+//   let numArr = String(num).split('').map(item => Number(item))
+//   let nextNum
+//   let i = 0
+//     while(true) {
+//     i ++
+//     nextNum = numArr.reduce((result, next) => result * next)
+//     if (nextNum.toString().length == 1) return i
+//     numArr = String(nextNum).split('').map(item => Number(item))
+//     }
+//   }
+// }
+
+// --- Best Practice :
+//function persistence(num) {
+//   var times = 0;
+
+//   num = num.toString();
+
+//   while (num.length > 1) {
+//     times++;
+//     num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+//   }
+
+//   return times;
+// }
+
+// --- Comment :
+
+//          ----           ----           -----           ----            ----            ----            ----
--- rank-up.js
+++ rank-up.js
@@ -874,6 +874,47 @@
 //          ----           ----           -----           ----            ----            ----            ---- 
 
 
-const arr = ["2", "+", "?"]
-const newArr = arr[2].replace("?", "3")
-console.log(arr)
+// --- 21 Feb 23 ---
+// --- Instruction : 
+//To give credit where credit is due: This problem was taken from the ACMICPC-Northwest Regional Programming Contest. Thank you problem writers.
+
+// You are helping an archaeologist decipher some runes. He knows that this ancient society used a Base 10 system, and that they never start a number with a leading zero. He's figured out most of the digits as well as a few operators, but he needs your help to figure out the rest.
+
+// The professor will give you a simple math expression, of the form
+
+// [number][op][number]=[number]
+// He has converted all of the runes he knows into digits. The only operators he knows are addition (+),subtraction(-), and multiplication (*), so those are the only ones that will appear. Each number will be in the range from -1000000 to 1000000, and will consist of only the digits 0-9, possibly a leading -, and maybe a few ?s. If there are ?s in an expression, they represent a digit rune that the professor doesn't know (never an operator, and never a leading -). All of the ?s in an expression will represent the same digit (0-9), and it won't be one of the other given digits in the expression. No number will begin with a 0 unless the number itself is 0, therefore 00 would not be a valid number.
+
+// Given an expression, figure out the value of the rune represented by the question mark. If more than one digit works, give the lowest one. If no digit works, well, that's bad news for the professor - it means that he's got some of his runes wrong. output -1 in that case.
+
+// Complete the method to solve the expression to find the value of the unknown rune. The method takes a string as a paramater repressenting the expression and will return an int value representing the unknown rune or -1 if no such rune exists.
+// --- My Solution : 
+// 
+// function solveExpression(exp) {
+//   const mathArr = exp.split('=')
+//   const op = mathArr[0].split('')
+//   const result = mathArr[1]
+//   const possibleNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//   if (op.includes("+")) {
+//     const leftOp = op.filter((item, index) => index < op.indexOf("+"))
+//     const rightOp = op.filter((item, index) => index > op.indexOf("+"))
+//   }
+//   else if (op.includes("*")) {
+//     const leftOp = op.filter((item, index) => index < op.indexOf("*"))
+//     const rightOp = op.filter((item, index) => index > op.indexOf("*"))
+//   }
+//   else {
+    
+//   }
+  
+//   for (num of possibleNum){
+    
+//   }
+// }
+
+// --- Best Practice : 
+// 
+
+// --- Comment : 
+
+//          ----           ----           -----           ----            ----            ----            ---- 
