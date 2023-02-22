18271a12a12dd0b5bb0800526c53aeadbc6b82d6
Hosaro <sty.hoareau@gmail.com>
2023-02-22 11:12:59 +0100

    22 feb 23 (fondamentals 2)

--- fondamentals.js
+++ fondamentals.js
@@ -237,3 +237,41 @@
 // --- Comment :
 
 //          ----           ----           -----           ----            ----            ----            ----
+
+// --- 22 Feb 23 ---
+// --- Instruction :
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+// --- My Solution :
+// function order(words){
+//   if (words == "") return ""
+//   else {
+//     const wordArr = words.split(" ")
+//     const finalArr = []
+//     for (let i=1; i<=wordArr.length; i++) {
+//       finalArr.push(wordArr.filter(item => item.includes(i)))
+//     }
+//     return finalArr.join(" ")
+//   }
+// }
+// --- Best Practice :
+// function order(words){
+
+//   return words.split(' ').sort(function(a, b){
+//     return a.match(/\d/) - b.match(/\d/);
+//  }).join(' ');
+// }
+//
+
+// --- Comment :
+// No best practices without regex.
+//          ----           ----           -----           ----            ----            ----            ----
