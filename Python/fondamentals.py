141c7c05ef84506714dc5e17c98ae489d69f98ea
Futur - Panda <sty.hoareau@gmail.com>
2023-04-16 15:00:07 +0200

    16 April Py (rank-up)

--- Python/fondamentals.py
+++ Python/fondamentals.py
@@ -381,15 +381,53 @@
 
 
 # ---- ---- ----- ---- ---- ---- ----
-def productFib(prod):
-    fib = [0, 1]
-    while fib[0]*fib[1] <= prod :
-        if fib[0]*fib[1] == prod : return [fib[0], fib[1], True]
-        current_num = fib[0] + fib[1]
-        fib = fib[1:]
-        fib.append(current_num)
-    return [fib[0], fib[1], False]
+
+# --- 31 March ---
+
+# --- Instructions ---
+# The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+# such as
+
+# F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+# Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+# F(n) * F(n+1) = prod.
+
+# Your function productFib takes an integer (prod) and returns an array:
+
+# [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+# depending on the language if F(n) * F(n+1) = prod.
+
+# If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+# [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+# F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+# --- My Solution
+# def productFib(prod):
+#     fib = [0, 1]
+#     while fib[0]*fib[1] <= prod :
+#         if fib[0]*fib[1] == prod : return [fib[0], fib[1], True]
+#         current_num = fib[0] + fib[1]
+#         fib = fib[1:]
+#         fib.append(current_num)
+#     return [fib[0], fib[1], False]
+
+# --- Best Practice ---
+
+# def productFib(prod):
+#   a, b = 0, 1
+#   while prod > a * b:
+#     a, b = b, a + b
+#   return [a, b, prod == a * b]
+
+# --- Comments ---
 
 
-print(productFib(4895)) #, [55, 89, True]
-print(productFib(5895)) #, [89, 144, False]
+# ---- ---- ----- ---- ---- ---- ----
+
+
